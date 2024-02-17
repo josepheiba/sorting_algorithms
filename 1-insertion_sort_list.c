916e90937432ce1871c8475e3a6fe2c58e5bcb41
Joseph Eiba <josepheiba@icloud.com>
2024-02-17 04:16:22 +0100

    auto_commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,6 +10,11 @@ void insertion_sort_list(listint_t **list)
 {
   listint_t *ll_ptr, *ll_ptr_idx; 
 
+  if ((*list)->next == NULL || *list == NULL)
+  {
+    return;
+  }
+
   ll_ptr = (*list)->next;
   while (ll_ptr != NULL)
   {
