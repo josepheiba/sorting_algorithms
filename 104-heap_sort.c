f275b0a3c20b96fc06091b515a3818bdae0f6d30
Joseph Eiba <josepheiba@icloud.com>
2024-02-20 00:51:08 +0100

    auto_commit

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,57 +1,66 @@
+#include <stdio.h>
 #include "sort.h"
 
 /**
- * heap_sort - order using Heap sort algorithm
+ * swap - Swaps two integers in an array
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+/**
+ * sift_down - Performs the sift-down operation on a binary heap
+ * @array: Array to be sorted
+ * @start: Index of the root of the heap
+ * @end: Index of the end of the heap
+ * @size: Size of the array
+ */
+void sift_down(int *array, size_t start, size_t end, size_t size)
+{
+    size_t root = start;
+    while ((root * 2) + 1 <= end) {
+        size_t child = (root * 2) + 1;
+        size_t swap_index = root;
+
+        if (array[swap_index] < array[child])
+            swap_index = child;
+
+        if (child + 1 <= end && array[swap_index] < array[child + 1])
+            swap_index = child + 1;
+
+        if (swap_index == root)
+            return;
+        
+        swap(&array[root], &array[swap_index]);
+        print_array(array, size);
+        root = swap_index;
+    }
+}
+
+/**
+ * heap_sort - Sorts an array of integers in ascending order using Heap sort algorithm
  * @array: Array to be sorted
  * @size: Size of the array
  */
 void heap_sort(int *array, size_t size)
 {
-	int i;
+  int i;
+  size_t j;
+    if (array == NULL || size < 2)
+        return;
 
-	for (i = size / 2 - 1; i >= 0; i--)
-	{
-		heapify(array, size, i);
-		print_array(array, size);
-	}
+    for (i = size / 2 - 1; i >= 0; i--) {
+        sift_down(array, i, size - 1, size);
+    }
 
-	for (i = size - 1; i > 0; i--)
-	{
-		int temp = array[0];
-
-		array[0] = array[i];
-		array[i] = temp;
-		print_array(array, size);
-		heapify(array, i, 0);
-		print_array(array, size);
-	}
-}
-
-/**
- * heapify - Repair the heap whose root element is at index 'i'
- * @array: Array to be sorted
- * @size: Size of the array
- * @i: Index of the root element of the subtree
- */
-void heapify(int *array, size_t size, size_t i)
-{
-	int temp;
-
-	size_t largest = i;
-	size_t left = 2 * i + 1;
-	size_t right = 2 * i + 2;
-
-	if (left < size && array[left] > array[largest])
-		largest = left;
-
-	if (right < size && array[right] > array[largest])
-		largest = right;
-
-	if (largest != i)
-	{
-		temp = array[i];
-		array[i] = array[largest];
-		array[largest] = temp;
-		heapify(array, size, largest);
-	}
+    for (j = size - 1; j > 0; j--) {
+        swap(&array[0], &array[j]);
+        print_array(array, size);
+        sift_down(array, 0, j - 1, size);
+    }
 }
