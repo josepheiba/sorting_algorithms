4b1750aa316ee72a210ebdac3c673004fa43ce59
Joseph Eiba <josepheiba@icloud.com>
2024-02-19 06:55:26 +0100

    auto_commit

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 #include <stdio.h>
-#include <sys/_types/_size_t.h>
 
 /**
  * quick_sort - Prints an array of integers
@@ -10,7 +9,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-  q_sort(array, 0, size - 1, size);
+  q_sort(array, 0, (int)(size - 1), size);
 }
 
 /**
@@ -21,14 +20,14 @@ void quick_sort(int *array, size_t size)
  * @r: The array to be printed
  * @size: The array to be printed
  */
-void q_sort(int *array, size_t l, size_t r, size_t size)
+void q_sort(int *array, int l, int r, size_t size)
 {
-  size_t p;
+  int p;
 
   if (l >= r)
     return;
   p = partition(array, l, r, size);
-  if (p != 0)
+  if (l != 0)
     q_sort(array, l, p - 1, size);
   q_sort(array, p + 1, r, size);
 }
@@ -41,15 +40,15 @@ void q_sort(int *array, size_t l, size_t r, size_t size)
  * @r: The array to be printed
  * @size: The array to be printed
  */
-size_t partition(int *array, size_t l, size_t r, size_t size)
+int partition(int *array, int l, int r, size_t size)
 {
-  size_t pivot, i, j, tmp;
+  int pivot, i, j, tmp;
 
-  pivot = (size_t)array[r];
+  pivot = array[r];
   j = l;
   for (i = l; i < r; i++)
   {
-    if ((size_t)array[i] < pivot)
+    if (array[i] < pivot)
     {
       tmp = array[i];
       array[i] = array[j];
