494c112dfd1d28d3e2847e1af0ac682492384e19
Joseph Eiba <josepheiba@icloud.com>
2024-02-19 13:16:39 +0100

    auto_commit

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,14 +1,35 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Prints an array of integers
+ * shell_sort - Prints an array of integers
  *
  * @array: The array to be printed
  * @size: Number of elements in @array
  */
 void shell_sort(int *array, size_t size)
 {
-  int i, j, outer_layer;
+  size_t i, j, interval;
+  int temp;
 
+  if (array == NULL || size < 2)
+	  return;
 
+  interval = 0;
+  while (interval < size / 3)
+		interval = (interval * 3) + 1;
+
+	while (interval != 0)
+	{
+		for (i = interval; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
+			{
+				array[j] = array[j - interval];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+		interval /= 3;
+	}
 }
