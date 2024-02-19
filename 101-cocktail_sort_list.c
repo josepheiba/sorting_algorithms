58c0e65da27671cb20a49e699168e9a9913d74ff
Joseph Eiba <josepheiba@icloud.com>
2024-02-19 15:16:51 +0100

    auto_commit

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -42,7 +42,7 @@ void cocktail_sort_list(listint_t **list)
 		}
 		ll_ptr = ll_ptr->next;
 		left_ptr = ll_ptr;
-	} while (left_ptr != right_ptr && left_ptr->next != right_ptr);
+	} while (left_ptr != right_ptr);
 }
 
 /**
