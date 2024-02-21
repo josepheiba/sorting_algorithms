d0c6a8fcc905e606851031a44094a1f5b6213daf
Joseph Eiba <josepheiba@icloud.com>
2024-02-20 18:17:23 -0800

    betty_102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,44 +10,33 @@ void counting_sort(int *array, size_t size)
 {
 	size_t i;
 	int max_num, current_num, duplicate;
-	int *counts;
-	int *sorted_array;
+	int *counts, *sorted_array;
 
 	if (array == NULL || size < 2)
 		return;
-
 	max_num = array[0];
 	for (i = 1; i < size; i++)
-	{
 		if (array[i] > max_num)
 			max_num = array[i];
-	}
-
 	counts = malloc(sizeof(int) * (size_t)(max_num + 1));
 	if (counts == NULL)
 		return;
-
 	for (i = 0; i < (size_t)(max_num + 1); i++)
 		counts[i] = 0;
-
 	for (i = 0; i < size; i++)
 	{
 		current_num = array[i];
 		counts[current_num]++;
 	}
-
 	for (i = 1; i < (size_t)(max_num + 1); i++)
 		counts[i] += counts[i - 1];
-
 	print_array(counts, max_num + 1);
-
 	sorted_array = malloc(sizeof(int) * size);
 	if (sorted_array == NULL)
 	{
 		free(counts);
 		return;
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		current_num = array[i];
@@ -55,10 +44,8 @@ void counting_sort(int *array, size_t size)
 		sorted_array[duplicate] = current_num;
 		counts[current_num]--;
 	}
-
 	for (i = 0; i < size; i++)
 		array[i] = sorted_array[i];
-
 	free(sorted_array);
 	free(counts);
 }
